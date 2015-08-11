56e9f7aa27ed18776de6d712686b57606c8ff0fb
Shahin <shahin@viator.com>
2015-08-11 13:31:13 +1000

    basic config

--- src/main/java/com/viator/tgds/App.java
+++ src/main/java/com/viator/tgds/App.java
@@ -1,6 +1,7 @@
 package com.viator.tgds;
 
 import com.typesafe.config.ConfigValueFactory;
+import com.viator.tgds.controller.Application;
 import org.jooby.Jooby;
 import org.jooby.Results;
 import org.jooby.ftl.Ftl;
@@ -14,7 +15,8 @@ public class App extends Jooby {
     use(new Ftl());
     assets("/assets/**");
 
-    get("/", req -> Results.html("index").put("appName",ConfigValueFactory.fromAnyRef("app.name")));
+    //get("/", req -> Results.html("index").put("appName",ConfigValueFactory.fromAnyRef("app.name")));
+    use(Application.class);
   }
 
   public static void main(final String[] args) throws Exception {
